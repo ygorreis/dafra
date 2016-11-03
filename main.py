08cf6a0bac14da08f0d6d45d9065bbb873ce5b75
Ygor Reis <Ygor Reis>
2016-11-03 02:25:16 -0200

    Remoção de acentuação

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import unicodedata
 from nltk.corpus import stopwords
 
 class ApplicationFramework:
@@ -19,7 +20,7 @@ class ApplicationFramework:
 	]
 	#END GLOBAL VAR AREA
 	
-	
+	#METHODS
 	def tokenizer(self, tweet):
 		tokens_re = re.compile(r'('+'|'.join(regex)+')', re.VERBOSE | re.IGNORECASE)
 		tokens = tokens_re.findall(tweet)
@@ -33,12 +34,21 @@ class ApplicationFramework:
 	
 		return list_tokens
 			
-	def to_lowercase(self, list_texts):
-		for item in list_texts:
-			item.lower()
-		
-		return list_texts
+	def to_lowercase(self, texts):
+		return texts.lower()
 				
+				
+	def remove_special_chars(char):
+		# https://gist.github.com/boniattirodrigo/67429ada53b7337d2e79
+		# Unicode normalize transforma um caracter em seu equivalente em latin.
+		
+		nfkd = unicodedata.normalize('NFKD', char)
+		new_char = u"".join([c for c in nfkd if not unicodedata.combining(c)])
+
+		# Usa expressão regular para retornar a palavra apenas com números, letras e espaço
+		return re.sub('[^a-zA-Z0-9 \\\]', '', new_char)
+		
+		
 class MyApp:
 	
 	#initialize framework
@@ -49,16 +59,18 @@ class MyApp:
 	
 	#tweet and stopword example
 	tweet = ['RT @marcobonzanini: just an example! :D http://example.com #NLP']
-	test_stopword = [':','!', 'RT']
+	test_stopword = [':','!', 'rt']
 
 	#to lowercase tweet
-	tweet = appFra.to_lowercase(tweet)
-	print "post lowercase"
-	print tweet
+	#change to pass and receive list
+	tweet[0] = appFra.to_lowercase(tweet[0])
 	
 	#tokenize tweet
 	tweet_tokenized.append(appFra.tokenizer(tweet[0]))
 	
-	print(appFra.remove_stopwords(test_stopword, tweet_tokenized))
+	#remove stopwords
+	tweet = appFra.remove_stopwords(test_stopword, tweet_tokenized)
+	
+	print tweet
 		
 MyApp()
